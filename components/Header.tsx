22a6be292dba0293c96d7b00a4f450045694cf43
Bem <minhhieu21193@gmail.com>
2022-11-18 15:53:30 +0700

    First Commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -47,7 +47,6 @@ function Header() {
             src="https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
             alt=""
             className="cursor-pointer rounded"
-            layout="fill"
           />
         </Link>
       </div>
